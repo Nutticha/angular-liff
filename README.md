c5ab3907ec43304cda2d0bdbb2d6ba121796ae85
Nutticha <52158825+Nutticha@users.noreply.github.com>
2023-02-15 14:06:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-liff
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-k6wpyn)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,11 @@
+import { from } from 'rxjs';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: '<h1>Hello {{name}}!</h1>',
+  styles: [`h1 { font-family: Lato; }`],
+})
+export class HelloComponent {
+  @Input() name: string;
+}
